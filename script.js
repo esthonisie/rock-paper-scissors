ac2ea44bae017126bb46112f3877e55629d6984e
esthonisie <132005743+esthonisie@users.noreply.github.com>
2023-06-13 15:41:26 +0200

    Create function to capitalize string user input

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function getComputerChoice() {
+const computerSelection = getComputerSelection();
+const playerSelection = capitalize("rOcK");
+
+function capitalize(input) {
+  const lowercase = input.toLowerCase();
+  return (lowercase.charAt(0).toUpperCase() + lowercase.slice(1));
+}
+
+function getComputerSelection() {
   const arrRPS = ["Rock", "Paper", "Scissors"];
   return arrRPS[(Math.floor(Math.random() * arrRPS.length))];
 }
 
-console.log(getComputerChoice());
+console.log(computerSelection);
+console.log(playerSelection);
+
