9aab7acbf7f925c0aabcab367edaefce4c49fbf6
esthonisie <132005743+esthonisie@users.noreply.github.com>
2023-06-13 20:15:52 +0200

    Create function to declare winner after 1 round

--- script.js
+++ script.js
@@ -11,6 +11,25 @@ function getComputerSelection() {
   return arrRPS[(Math.floor(Math.random() * arrRPS.length))];
 }
 
-console.log(computerSelection);
-console.log(playerSelection);
+function compareSelection(you, other) {
+  if(you === "Rock" && other === "Paper") {
+      return "Paper beats Rock.\nYou've lost this round!";
+  } else if(you === "Rock" && other === "Scissors") {
+      return "Rock beats Scissors.\nYou've won this round!"; 
+  } else if(you === "Paper" && other === "Rock") {
+      return "Paper beats Rock.\nYou've won this round!";
+  } else if(you === "Paper" && other === "Scissors") {
+      return "Scissors beats Paper.\nYou've lost this round!";
+  } else if(you === "Scissors" && other === "Rock") {
+      return "Rock beats Scissors.\nYou've lost this round!";
+  } else if(you === "Scissors" && other === "Paper") {
+      return "Scissors beats Paper.\nYou've won this round!";
+  } else if(you === other) {
+      return "It's a tie!";
+  } 
+}
+
+console.log(`Your Choice: ${playerSelection}`);
+console.log(`Computer's Choice: ${computerSelection}`);
+console.log(compareSelection(playerSelection, computerSelection))
 
