a9517231020778e6629472b1cdcad8b8938cb0b8
esthonisie <132005743+esthonisie@users.noreply.github.com>
2023-06-14 11:39:04 +0200

    Add for loop to play 5 rounds

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
-let playerPoints = 0;
-let computerPoints = 0;
-let roundResult = "";
+playGame();
 
-const computerSelection = getComputerSelection();
-const playerSelection = capitalize(prompt("Rock, Paper, Scissors. Your choice:"));
+function playGame() {
 
-function capitalize(input) {
-  const lowercase = input.toLowerCase();
-  return (lowercase.charAt(0).toUpperCase() + lowercase.slice(1));
-}
+  let playerPoints = 0;
+  let computerPoints = 0;
+  let roundResult = "";
 
-function getComputerSelection() {
-  const arrRPS = ["Rock", "Paper", "Scissors"];
-  return arrRPS[(Math.floor(Math.random() * arrRPS.length))];
-}
+  for (let i = 0; i < 5; i++) {
+    playRound();
+  }
 
-function compareSelection(you, other) {
-  if(you === "Rock" && other === "Paper") {
-      roundResult = "Paper beats Rock.\nYou've lost this round!";
-      computerPoints += 10;
-  } else if(you === "Rock" && other === "Scissors") {
-      roundResult = "Rock beats Scissors.\nYou've won this round!"; 
-      playerPoints += 10;
-  } else if(you === "Paper" && other === "Rock") {
-      roundResult = "Paper beats Rock.\nYou've won this round!";
-      playerPoints += 10;
-  } else if(you === "Paper" && other === "Scissors") {
-      roundResult = "Scissors beats Paper.\nYou've lost this round!";
-      computerPoints += 10;
-  } else if(you === "Scissors" && other === "Rock") {
-      roundResult = "Rock beats Scissors.\nYou've lost this round!";
-      computerPoints += 10;
-  } else if(you === "Scissors" && other === "Paper") {
-      roundResult = "Scissors beats Paper.\nYou've won this round!";
-      playerPoints += 10;
-  } else if(you === other) {
-      roundResult = "It's a tie!";
-      playerPoints += 5;
-      computerPoints += 5;
-  } else if(you === "" || (you !== "Rock" || you !== "Paper" || you !== "Scissors")) {
-      roundResult = "Hello User. Please make a choice: Rock, Paper or Scissors.";
+  function playRound() {
+    const computerSelection = getComputerSelection();
+    const playerSelection = capitalize(prompt("Rock, Paper, Scissors. Your choice:"));
+
+    function capitalize(input) {
+      const lowercase = input.toLowerCase();
+      return (lowercase.charAt(0).toUpperCase() + lowercase.slice(1));
+    }
+
+    function getComputerSelection() {
+      const arrRPS = ["Rock", "Paper", "Scissors"];
+      return arrRPS[(Math.floor(Math.random() * arrRPS.length))];
+    }
+
+    function compareSelection(you, other) {
+      if(you === "Rock" && other === "Paper") {
+          roundResult = "Paper beats Rock.\nYou've lost this round!";
+          computerPoints += 10;
+      } else if(you === "Rock" && other === "Scissors") {
+          roundResult = "Rock beats Scissors.\nYou've won this round!"; 
+          playerPoints += 10;
+      } else if(you === "Paper" && other === "Rock") {
+          roundResult = "Paper beats Rock.\nYou've won this round!";
+          playerPoints += 10;
+      } else if(you === "Paper" && other === "Scissors") {
+          roundResult = "Scissors beats Paper.\nYou've lost this round!";
+          computerPoints += 10;
+      } else if(you === "Scissors" && other === "Rock") {
+          roundResult = "Rock beats Scissors.\nYou've lost this round!";
+          computerPoints += 10;
+      } else if(you === "Scissors" && other === "Paper") {
+          roundResult = "Scissors beats Paper.\nYou've won this round!";
+          playerPoints += 10;
+      } else if(you === other) {
+          roundResult = "It's a tie!";
+          playerPoints += 5;
+          computerPoints += 5;
+      } else if(you === "" || (you !== "Rock" || you !== "Paper" || you !== "Scissors")) {
+          roundResult = "Hello User. Please make a choice: Rock, Paper or Scissors.";
+      }
+    }
+
+    compareSelection(playerSelection, computerSelection);
+
+    alert(`Your Choice: ${playerSelection}  |  Computer's Choice: ${computerSelection}\n${roundResult}\n\nYour Score: ${playerPoints} points  |  Computer's Score: ${computerPoints} points`);
+    
   }
 }
 
-compareSelection(playerSelection, computerSelection);
 
-alert(`Your Choice: ${playerSelection}  |  Computer's Choice: ${computerSelection}\n${roundResult}\n\nYour Score: ${playerPoints} points  |  Computer's Score: ${computerPoints} points`);
+
 
