b459130e3c4ed522ecc78ecf5278fa40573845b6
esthonisie <132005743+esthonisie@users.noreply.github.com>
2023-06-14 10:41:43 +0200

    Add points variables for scoring system

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerPoints = 0;
+let computerPoints = 0;
 let roundResult = "";
 
 const computerSelection = getComputerSelection();
@@ -16,18 +18,26 @@ function getComputerSelection() {
 function compareSelection(you, other) {
   if(you === "Rock" && other === "Paper") {
       roundResult = "Paper beats Rock.\nYou've lost this round!";
+      computerPoints += 10;
   } else if(you === "Rock" && other === "Scissors") {
       roundResult = "Rock beats Scissors.\nYou've won this round!"; 
+      playerPoints += 10;
   } else if(you === "Paper" && other === "Rock") {
       roundResult = "Paper beats Rock.\nYou've won this round!";
+      playerPoints += 10;
   } else if(you === "Paper" && other === "Scissors") {
       roundResult = "Scissors beats Paper.\nYou've lost this round!";
+      computerPoints += 10;
   } else if(you === "Scissors" && other === "Rock") {
       roundResult = "Rock beats Scissors.\nYou've lost this round!";
+      computerPoints += 10;
   } else if(you === "Scissors" && other === "Paper") {
       roundResult = "Scissors beats Paper.\nYou've won this round!";
+      playerPoints += 10;
   } else if(you === other) {
       roundResult = "It's a tie!";
+      playerPoints += 5;
+      computerPoints += 5;
   } else if(you === "" || (you !== "Rock" || you !== "Paper" || you !== "Scissors")) {
       roundResult = "Hello User. Please make a choice: Rock, Paper or Scissors.";
   }
@@ -35,5 +45,5 @@ function compareSelection(you, other) {
 
 compareSelection(playerSelection, computerSelection);
 
-alert(`Your Choice: ${playerSelection}  |  Computer's Choice: ${computerSelection}\n${roundResult}`);
+alert(`Your Choice: ${playerSelection}  |  Computer's Choice: ${computerSelection}\n${roundResult}\n\nYour Score: ${playerPoints} points  |  Computer's Score: ${computerPoints} points`);
 
